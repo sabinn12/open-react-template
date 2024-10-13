26fa5c757b5dba733e940519a3254ed5f7d72774
pro <irakizasabin12@gmail.com>
2024-10-13 17:01:21 +0200

    updated

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -193,7 +193,7 @@ export default function Footer() {
                   className="text-indigo-200/65 transition hover:text-indigo-500"
                   href="#0"
                 >
-                  Cookie manager
+                  irakiza sabin
                 </a>
               </li>
             </ul>
